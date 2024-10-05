80b6695ff77c6a5363cb072a2410d36c214aa2d8
Kidron Lightfoot <kidron.light99@gmail.com>
2024-10-04 18:12:32 -0700

    creating the about page

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+function toggleSection(sectionId) {
+    const content = document.getElementById(sectionId);
+    if (content.style.display === "none" || content.style.display === "") {
+        content.style.display = "block";
+    } else {
+        content.style.display = "none";
+    }
+}
